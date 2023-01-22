df8904a730a198f0369f10f640365cf3b4250296
Bartek Pawlaczyk <bpawlaczyk1982@gmail.com>
2023-01-22 12:06:43 +0100

    deleted rates values from html code and moved them to script.js, also changed script.js (added switch and resultElement,innerHTML

--- js/script.js
+++ js/script.js
@@ -4,15 +4,37 @@ let currencyElement = document.querySelector(".js-currency");
 let button = document.querySelector(".js-button");
 let resultElement = document.querySelector(".js-result");
 
+let rateEUR = 4.6965;
+let rateUSD = 4.4828;
+let rateCHF = 4.7608;
+let rateGBP = 5.4389;
+
 formElement.addEventListener("submit", (event) => {
     event.preventDefault();
     const amount = +amountElement.value;
     const currency = currencyElement.value;
 
-    if (amount === "") {
-        return;
+    let rate;
+
+    switch (currency) {
+        case "EUR":
+            rate = rateEUR;
+            break;
+        
+        case "USD":
+            rate = rateUSD;
+            break;
+        
+        case "CHF":
+            rate = rateCHF;
+            break;
+        
+            case "GBP":
+            rate = rateGBP;
+            break;
     }
 
-    let result = amount * currency;
-    resultElement.innerText = result.toFixed(2);
+    const result = amount / rate;
+
+    resultElement.innerHTML = `${amount.toFixed(2)} PLN = <strong>${result.toFixed(2)} ${currency}</strong>`;
 });
